f4658d606462ff0ff7d42523a06208a6a994ef97
Andris Buscariolli <andris.contact@gmail.com>
2021-06-14 12:26:05 -0300

    Add: 		1. Setup css/sass 		2. Setup firebase 		3. Created BaseSpinner component

---
+++ src/components/global/BaseSpinner.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="base-spinner fa-3x" v-if="visible">
+    <i class="fas fa-circle-notch fa-spin"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      visible: false
+    }
+  },
+  created () {
+    this.$root.$on('Spinner::show', this.toogleSpinner)
+    this.$root.$on('Spinner::hide', this.toogleSpinner)
+  },
+  methods: {
+    toogleSpinner () {
+      this.visible = !this.visible
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+.base-spinner {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 1000000;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: $dark-medium;
+  color: $featured;
+}
+</style>
